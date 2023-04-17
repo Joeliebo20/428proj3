27c2fafdc443f39313d3818faf5550709c258395
Joseph Lieberman <54378952+Joeliebo20@users.noreply.github.com>
2023-04-17 13:35:42 -0400

    Update udppinger-client1.py

--- udppinger-client1.py
+++ udppinger-client1.py
@@ -1,6 +1,7 @@
 # We will need the following module to generate randomized lost packets
 import random
 import time
+import threading
 
 # Import socket module
 from socket import *
@@ -17,8 +18,32 @@ print(f'host: {host}')
 serverSocket.bind((host, port))
 
 
+
 client = socket(AF_INET, SOCK_DGRAM)
+client.settimeout(1) # wait up to one second
+sequence_number = 1
+packets_sent = 0
+packets_received = 0
+
+total_time = 180 # 3 minutes in seconds
+
+# no need for host, port since this is only a pinger
+
 start = time.time()
+
+while time.time() - start < 180:
+    msg = f'ping {sequence_number} sent successfully at {time.time()}'
+
+    try:
+        server_info = (host, port)
+        client.sendto(msg, server_info)
+        packets_sent += 1
+        sequence_number += 1
+
+        resp, addr = client.recvfrom(1024)
+        packets_received += 1
+    except:
+        
 
 
 while True:
